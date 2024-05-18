f7ed1ad3ef13857151373c9b0c1c7c306d33b3ba
KayceeXclusive <dxclusive600@gmail.com>
2024-05-18 14:04:18 +0100

    change theme, add clock, handle edit and complete error

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
 import functions
-
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("Black")
+
+clock = sg.Text('', key='clocks')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -12,20 +15,24 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
 
                    font=('Helvetica', 15))
 while True:
+
+    event, values = window.read(timeout=10)
+    window["clocks"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    # --------------------------------------------
     # here we are going to collect the
     # event which is the button clicked and the
     # value which is the word entered
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    # print(1, event)
+    # print(2, values)
+    # print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -36,24 +43,33 @@ while True:
             window['todos'].update(values=todos)
             window['todo'].update(value='')
         case "Edith":
-            todo_to_edith = values['todos'][0]
-            new_todo = values['todo'] + "\n"
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edith)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            #this will help to update the list in realtime
-            window['todos'].update(values=todos)
-        # here we want when we click let the clicked item show on the input before editing
+            try:
+                todo_to_edith = values['todos'][0]
+                new_todo = values['todo'] + "\n"
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edith)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                #this will help to update the list in realtime
+                window['todos'].update(values=todos)
+            # here we want when we click let the clicked item show on the input before editing
+            except IndexError:
+                sg.popup("Please select an Item", font=("Helvetica", 20))
+
+
         case 'Complete':
-            # we are writing this very line to get the particular todos from the list. remember the todos is a refrence key in line 8
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            # here we are make sure the value is no more on the input after removing it from the todo list
-            window['todo'].update(value='')
+            try:
+                # we are writing this very line to get the particular todos from the list. remember the todos is a refrence key in line 8
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                # here we are make sure the value is no more on the input after removing it from the todo list
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an Item", font=("Helvetica", 20))
+
         case 'todos':
             # remember here we are making anything we click to also appear on input
             window['todo'].update(value=values['todos'][0])
